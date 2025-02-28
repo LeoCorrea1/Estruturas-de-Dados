ca67172132f82232226d27212883d87c169097d0
Leonardo Correa <thefuzileirobr@gmail.com>
2025-02-28 09:58:37 -0300

    Update AULA 2 - Codigo que popula uma lista com n numeros aleatorios inteiros.java

--- AULA 2 - Codigo que popula uma lista com n numeros aleatorios inteiros.java
+++ AULA 2 - Codigo que popula uma lista com n numeros aleatorios inteiros.java
@@ -5,37 +5,47 @@ import java.util.Random;
 
 public class Aula2 {
     public static void main(String[] args) {
-        //codigo que popula uma lista com n numeros aleatorios inteiros
+        // Codigo que popula uma lista com números aleatórios inteiros
         ArrayList<Integer> lista = new ArrayList<>();
-        
         Random gerador = new Random();
-        int tamanho = gerador.nextInt(10,20);
+        int tamanho = gerador.nextInt(10, 20);  // Tamanho da lista (entre 10 e 19)
         int numero;
-        
+
         // Inserir números aleatórios na lista
-        for(int i = 0; i < tamanho; i++) {
+        for (int i = 0; i < tamanho; i++) {
             numero = gerador.nextInt(1000);  // Gera um número aleatório de 0 a 999
-            lista.add(numero);
+            if (!lista.contains(numero)) {
+                lista.add(numero);  // Só adiciona se o número não existir na lista
+            }
         }
+        
+        //ordenar
+        lista.sort(null);
         
         // Exibir por índices
-        System.out.println("EXIBINDO POR INDICES");
+        System.out.println("EXIBINDO POR ÍNDICES");
         for (int i = 0; i < lista.size(); i++) {
             System.out.println(lista.get(i));
+            
         }
-        
+        System.out.println("TOTAL DE " +lista.size() + "numeros\n\n");
+
         // Exibir por objetos
         System.out.println("EXIBINDO POR OBJETOS");
-        for(Integer i : lista) {
+        for (Integer i : lista) {
             System.out.println(i);
+            
         }
+        System.out.println("TOTAL DE " +lista.size() + "numeros\n\n");
         
-        // Exibir Usando Iterator
+        // Exibir usando Iterator
         System.out.println("USANDO ITERATOR");
         Iterator<Integer> iterator = lista.iterator();
-        while(iterator.hasNext()) {
+        
+        while (iterator.hasNext()) {
             numero = iterator.next();
             System.out.println(numero);
         }
+        System.out.println("TOTAL DE " +lista.size() + "numeros\n\n");
     }
 }
