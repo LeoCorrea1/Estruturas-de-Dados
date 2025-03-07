f96ff5902f92293ff4ade9d1a8c48cd6392eeac7
Leonardo Correa <leonardo.vasconcelos@ufn.edu.br>
2025-03-07 09:40:25 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿List<string> ListaNomes = new List<string>();
+string opcao;
+string nome;
+
+do
+{
+    Console.Write("digite um nome completo : ");
+    nome = Console.ReadLine();
+    nome = nome.ToUpper();
+
+    if (!ListaNomes.Contains(nome))
+    {
+     ListaNomes.Add(nome);  
+    }else
+    {
+        Console.WriteLine("Nome ja cadastrado");
+    }
+    Console.WriteLine("1 - continua , 2 - finaliza");
+    opcao = Console.ReadLine();
+
+}while (opcao != "2");
+
+ListaNomes.Sort();
+Console.WriteLine("A lista Contem : " +ListaNomes.Count);
+foreach (var i in ListaNomes)
+{
+    Console.WriteLine(i);
+}
+     
